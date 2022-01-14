b0231458f801d12a5a779cd4f2c09dbb0499e150
lsmiley <lsmiley@us.ibm.com>
2022-01-13 21:47:49 -0500

    Merge remote-tracking branch 'origin/master'

--- mysite/urls.py
+++ mysite/urls.py
@@ -78,4 +78,4 @@ urlpatterns = [
     path('saveorderitem', apiViews.saveorderitem, name='saveorderitem'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
