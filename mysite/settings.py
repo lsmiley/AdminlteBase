850229098428e30a2c128b5f356de1e7de52605e
lsmiley <lsmiley@us.ibm.com>
2022-05-03 12:53:09 -0400

    Merge remote-tracking branch 'origin/master'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x&7$)!$nj9#ca(3s%xln!m_^18_$+=5nt$z8s^chlntq570m6u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
